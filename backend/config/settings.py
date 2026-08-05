94405517a402d0f366d9838388c7344d14bb740f
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-08-05 18:58:10 +0100

    Fix production allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,10 +34,14 @@ if DEBUG:
     ALLOWED_HOSTS = [
         "127.0.0.1",
         "localhost",
-        "uniagora-landing.onrender.com"
+    ]
+else:
+    ALLOWED_HOSTS = [
+        "uniagora-landing.onrender.com",
         "uniagora.shop",
         "www.uniagora.shop",
     ]
+
 # ------------------------------------------------------------------------------
 # CSRF
 # ------------------------------------------------------------------------------
